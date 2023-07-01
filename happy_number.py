004fcaeb5c6699e09c2a83e30290ec7e5cee973b
Manikantagowtham <113998595+Manikantagowtham@users.noreply.github.com>
2023-07-01 10:18:40 +0530

    def isHappyNumber(num):     rem = sum = 0     while(num > 0):         rem = num%10         sum = sum + (rem*rem)         num = num//10     return sum
    num = int(input())    
    result = num    
         
    while(result != 1 and result != 4):    
        result = isHappyNumber(result)  
            
    if(result == 1):    
        print( True)      
    elif(result == 4):    
        print(False)

--- happy_number.py
+++ happy_number.py
@@ -1,13 +1,18 @@
-n=int(input())
-s=0
-while n>0:
-    r=n%10
-    s=s+r*r
-    n=n//10
-    if n==0 and s>9:
-        n=s
-        s=0
-if s==1 or s==7:
-    print(True)
-else:
-    print(False)
+def isHappyNumber(num):    
+    rem = sum = 0    
+    while(num > 0):    
+        rem = num%10    
+        sum = sum + (rem*rem)    
+        num = num//10   
+    return sum    
+        
+num = int(input())    
+result = num    
+     
+while(result != 1 and result != 4):    
+    result = isHappyNumber(result)  
+        
+if(result == 1):    
+    print( True)      
+elif(result == 4):    
+    print(False)   
